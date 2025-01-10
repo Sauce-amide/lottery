585375651540da4e0af689fea202775d2469add6
Sauce <786314327@qq.com>
2025-01-10 15:52:32 +0800

    Update member.js

--- js/member.js
+++ js/member.js
@@ -1,29 +1,38 @@
 var member = [
   {
+    "phone":"",
     "name": "金额100"
   },
   {
+    "phone":"",
     "name": "金额200"
   },
   {
+    "phone":"",
     "name": "金额300"
   },
   {
+    "phone":"",
     "name": "金额400"
   },
   {
+    "phone":"",
     "name": "金额500"
   },
   {
+    "phone":"",
     "name": "金额600"
   },
   {
+    "phone":"",
     "name": "金额700"
   },
   {
+    "phone":"",
     "name": "金额800"
   },
   {
+    "phone":"",
     "name": "金额900"
   }
 ]
